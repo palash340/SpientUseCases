7ea3bf8621f82404b1d0bb0a70121d69a5549626
Palash Jain <palash340@gmail.com>
2017-10-09 23:57:30 +0530

    Initial Commit

--- src/main/java/sortfile/SortFiles.java
+++ src/main/java/sortfile/SortFiles.java
@@ -1,8 +1,11 @@
 package sortfile;
 
-import java.io.File;
-import java.io.FilenameFilter;
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 /**
@@ -13,13 +16,27 @@ public class SortFiles {
         SortFiles sf = new SortFiles();
         List<File> fileList = sf.listAllFiles("src/main/resources", "split");
         System.out.println("fileList = " + fileList);
+        sf.sortFile(fileList);
     }
 
     public void sortFile(List<File> fileList){
         fileList.forEach(file -> {
-
+            try{
+                // Read file and sort in-memory
+                List<String> wordList = new ArrayList<String>();
+                Files.lines(Paths.get(file.getAbsolutePath())).forEach(inputLine -> {
+                    String[] words = inputLine.split("\\s+");
+                    for(String word : words)
+                        wordList.add(word);
+                });
+                // Sort list
+                Collections.sort(wordList);
+                // Write to a temp file sorted file
+                System.out.println("wordList = " + wordList);
+            }catch(IOException ioe){
+                ioe.printStackTrace();
+            }
         });
-
     }
 
 
@@ -29,11 +46,8 @@ public class SortFiles {
 
     public List<File> listAllFiles(String filePath, String filePattern){
         File[] listOfFiles = new File(filePath).listFiles(new MyFileNameFilter(filePattern));
-        List<File> fileList  = Arrays.asList(listOfFiles);
-        return fileList;
+        return Arrays.asList(listOfFiles);
     }
-
-
 }
 
 class MyFileNameFilter implements FilenameFilter {
