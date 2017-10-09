98e6c266175a618065727c63e85c8aa7907f1044
Palash Jain <palash340@gmail.com>
2017-10-09 23:24:18 +0530

    Initial Commit

--- src/main/java/sortfile/SortFiles.java
+++ src/main/java/sortfile/SortFiles.java
@@ -1,7 +1,42 @@
 package sortfile;
 
+import java.io.File;
+import java.io.FilenameFilter;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
 /**
- * Created by administrator on 10/9/17.
+ * Created by Palash on 10/9/17.
  */
 public class SortFiles {
+    public static void main(String[] args) {
+        SortFiles sf = new SortFiles();
+        List<File> fileList = sf.listAllFiles("src/main/resources", "split");
+        System.out.println("fileList = " + fileList);
+    }
+
+    public List<File> listAllFiles(String filePath, String filePattern){
+        File[] listOfFiles = new File(filePath).listFiles(new MyFileNameFilter(filePattern));
+        List<File> fileList  = Arrays.asList(listOfFiles);
+        return fileList;
+    }
+
+
+}
+
+class MyFileNameFilter implements FilenameFilter {
+
+    private String prefix;
+
+    public MyFileNameFilter(String prefix) {
+        this.prefix = prefix.toLowerCase();
+    }
+
+    @Override
+    public boolean accept(File dir, String name) {
+        return name.toLowerCase().startsWith(prefix);
+    }
+
 }
